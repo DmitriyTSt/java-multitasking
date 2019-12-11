d3c7edb9230ecbe7962c41311a052ae6f59456e2
Dmitriy Tomilov <d.tomilov@profsoft.pro>
2019-12-11 23:02:17 +0400

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,78 @@
+import com.aparapi.Kernel
+import java.util.Arrays
+import java.util.stream.IntStream
+
+fun main() {
+    gpu()
+}
+
+fun gpu() {
+    val size = 100000
+    val a = IntStream.range(2, size + 2).toArray()
+    val primeNumbers = BooleanArray(size)
+
+    val kernel = object : Kernel() {
+        override fun run() {
+            val gid = globalId
+            val num = a[gid]
+            var prime = true
+            for (i in 2 until num) {
+                if (num % i == 0) {
+                    prime = false
+                    //break is not supported
+                }
+            }
+            primeNumbers[gid] = prime
+        }
+    }
+    val startTime = System.currentTimeMillis()
+    kernel.execute(size)
+    System.out.printf("time taken: %s ms%n", System.currentTimeMillis() - startTime)
+    println(Arrays.toString(primeNumbers.copyOf(20)))//just print a sub array
+    kernel.dispose()
+}
+
+fun cpuSingle() {
+    val size = 100000
+    val a = IntStream.range(2, size + 2).toArray()
+    val primeNumbers = BooleanArray(size)
+
+    val startTime = System.currentTimeMillis()
+    for (n in 0 until size) {
+        val num = a[n]
+        var prime = true
+        for (i in 2 until num) {
+            if (num % i == 0) {
+                prime = false
+                //not using break for a fair comparision
+            }
+        }
+        primeNumbers[n] = prime
+    }
+    System.out.printf("time taken: %s ms%n", System.currentTimeMillis() - startTime)
+    println(Arrays.toString(primeNumbers.copyOf(20)))//just print a sub array
+}
+
+fun cpuParallel() {
+    val size = 100000
+    val a = IntStream.range(2, size + 2).toArray()
+
+    val startTime = System.currentTimeMillis()
+    val primeNumbers = Arrays.stream(a)
+        .parallel()
+        .mapToObj { isPrime(it) }
+        .toArray()
+    System.out.printf("time taken: %s ms%n", System.currentTimeMillis() - startTime)
+    println(Arrays.toString(primeNumbers.copyOf<Any>(20)))//just print a sub array
+}
+
+fun isPrime(num: Int): Boolean {
+    var prime = true
+    for (i in 2 until num) {
+        if (num % i == 0) {
+            prime = false
+            //not using break for a fair comparision
+        }
+    }
+    return prime
+}
